ab1ce3cc5d34883bf0248e093673b9c5defd3e93
Oleg Sandro <okuziura@gmail.com>
2019-10-18 08:32:12 +0300

    GEOIN-1 Add endpoint, which returns position info by specified coords

--- src/main/java/com/example/geoinformer/controller/PositionController.java
+++ src/main/java/com/example/geoinformer/controller/PositionController.java
@@ -1,41 +1,73 @@
 package com.example.geoinformer.controller;
 
 import com.example.geoinformer.entity.Position;
-import com.example.geoinformer.entity.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class PositionController {
 
+    private final String SUCCESS = "Success!";
+    private final String FAILED = "Failed!";
+    private final String NULL = "Null!";
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Метод, служащий для проверки работы веб-сервиса
+     * @return объект – представление с текстом "Hello world"
+     */
     @GetMapping("/")
-    public String printSomething() {
-        return "Hello world";
+    public String index() {
+        String content = "Hello world";
+        logger.info(content);
+        return content;
     }
 
-    @GetMapping("/quot")
-    public ResponseEntity<Quote> qoute() {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://gturnquist-quoters.cfapps.io/api/random";
-        Quote quote = restTemplate.getForObject(url, Quote.class);
-        if(quote == null){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); //204
+    /**
+     * Метод, возвращающий по координатам WGS 84 все данные о найденном месте
+     * @param lat число – значение широты в градусах (в пределах от -90 до 90)
+     * @param lon число – значение долготы в градусах (в пределах от -180 до 180)
+     * @return объект – ответ от контроллера, включающий объект сущности Position и статус HTTP-запроса
+     */
+    @GetMapping("/pos-by-coords")
+    public ResponseEntity<Position> getPositionByCoords(@RequestParam Float lat, @RequestParam Float lon) {
+        if (
+            lat.compareTo(-90F) >= 0 &&
+            lat.compareTo(90F) <= 0 &&
+            lon.compareTo(-180F) >= 0 &&
+            lon.compareTo(180F) <= 0
+        ) {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = String.format("https://nominatim.openstreetmap.org/reverse?" +
+                    "email=okuziura@gmail.com&format=jsonv2&accept-language=ru&zoom=18&lat=%f&lon=%f", lat, lon);
+            logger.info(url);
+            Position position = restTemplate.getForObject(url, Position.class);
+            return returnResponse(position);
+        } else {
+            return returnResponse(null);
         }
-        return new ResponseEntity<>(quote, HttpStatus.OK); //200
     }
 
-    @GetMapping("/pos")
-    public ResponseEntity<Position> pos() {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://nominatim.openstreetmap.org/reverse?email=okuziura@gmail.com&format=json&lat=52.42107819733966&lon=31.015610763888095";
-        System.out.println(url);
-        Position position = restTemplate.getForObject(url, Position.class);
-        if(position == null){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); //204
+    /**
+     * Метод, позволяющий в зависимости от обнаружения места возвратить объект сущности Position и статус HTTP-запроса
+     * @param position объект – место на карте
+     * @return объект – ответ от контроллера, включающий объект сущности Position и статус HTTP-запроса
+     */
+    private ResponseEntity<Position> returnResponse(Position position) {
+        if (position == null) {
+            logger.warn(NULL);
+            logger.info(FAILED);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
         }
-        return new ResponseEntity<>(position, HttpStatus.OK); //200
+        logger.info(position.toString());
+        logger.info(SUCCESS);
+        return new ResponseEntity<>(position, HttpStatus.OK); // 200
     }
 }

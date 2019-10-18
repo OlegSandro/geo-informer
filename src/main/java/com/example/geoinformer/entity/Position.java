531b9eb4db2435ba14d5f5f5b81802206ef72e17
Oleg Sandro <okuziura@gmail.com>
2019-10-18 08:32:12 +0300

    GEOIN-1 Add endpoint, which returns position info by specified coords

--- src/main/java/com/example/geoinformer/entity/Position.java
+++ src/main/java/com/example/geoinformer/entity/Position.java
@@ -8,33 +8,114 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Position {
 
-    private int place_id;
-    private String osm_type;
+    private long id;
+    @JsonProperty("lat")
+    private float latitude;
+    @JsonProperty("lon")
+    private float longitude;
+    private String country; // 14 symbols
+    @JsonProperty("type")
+    private String type; // 22 symbols
+    @JsonProperty("display_name")
+    private String name;
+    @JsonProperty("osm_id")
+    private Long osmId;
+    @JsonProperty("osm_type")
+    private Character osmType;
 
     public Position() {
     }
 
-    public int getPlace_id() {
-        return place_id;
+    public long getId() {
+        return id;
     }
 
-    public void setPlace_id(int place_id) {
-        this.place_id = place_id;
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public String getOsm_type() {
-        return osm_type;
+    public float getLatitude() {
+        return latitude;
     }
 
-    public void setOsm_type(String osm_type) {
-        this.osm_type = osm_type;
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getOsmId() {
+        return osmId;
+    }
+
+    public void setOsmId(Long osmId) {
+        this.osmId = osmId;
+    }
+
+    public Character getOsmType() {
+        return osmType;
+    }
+
+    public void setOsmType(Character osmType) {
+        this.osmType = osmType;
+    }
+
+    public void setOsmType(String osmType) {
+        this.osmType = (osmType.equals("node"))
+                ? 'N'
+                : (osmType.equals("relation"))
+                ? 'R'
+                : (osmType.equals("way"))
+                ? 'W'
+                : null;
+    }
+
+    @JsonProperty("address")
+    private void unpackCountry(Map<String, Object> address) {
+        this.country = (String) address.get("country");
     }
 
     @Override
     public String toString() {
         return "Position{\n" +
-                "place_id=" + ",\n" +
-                "osm_type=\"" + "\"\n" +
+                "id=" + id + ",\n" +
+                "lat=" + latitude + ",\n" +
+                "lon=" + longitude + ",\n" +
+                "country=\"" + country + "\",\n" +
+                "type=\"" + type + "\",\n" +
+                "name=\"" + name + "\",\n" +
+                "osm_id=" + osmId + ",\n" +
+                "osm_type=\'" + osmType + "\'\n" +
                 "}";
     }
 }

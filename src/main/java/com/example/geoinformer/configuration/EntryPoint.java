81099904adc0d52b2837356ef8ec6d458adcf743
Oleg Sandro <okuziura@gmail.com>
2019-10-24 07:38:43 +0300

    GEOIN-7 Refactor package for configuration

--- src/main/java/com/example/geoinformer/configuration/EntryPoint.java
+++ src/main/java/com/example/geoinformer/configuration/EntryPoint.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = {"com.example.geoinformer.controller", "com.example.geoinformer.service"})
+@SpringBootApplication(scanBasePackages = {"com.example.geoinformer.service", "com.example.geoinformer.controller"})
 @EnableJpaRepositories(basePackages = "com.example.geoinformer.repository")
-@EnableTransactionManagement
 @EntityScan(basePackages = "com.example.geoinformer.entity")
-public class Application {
+public class EntryPoint {
 
-    public static void main(String args[]) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(EntryPoint.class, args);
     }
 }

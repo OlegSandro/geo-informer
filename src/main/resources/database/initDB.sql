1285b550b1c003eb5e415868702f09a2dc068fc6
Oleg Sandro <okuziura@gmail.com>
2019-10-30 13:25:35 +0300

    GEOIN-6 Fix SQL-script for DB creation

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -2,13 +2,13 @@ CREATE SCHEMA `map` DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `map`.`position` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `osm_type` VARCHAR(1) NULL,
+  `osm_id` BIGINT NULL,
   `latitude` REAL NOT NULL,
   `longitude` REAL NOT NULL,
-  `country` VARCHAR(14) NULL,
+  `country` VARCHAR(2) NULL,
   `type` VARCHAR(22) NULL,
-  `name` VARCHAR(150) NULL,
-  `osm_id` BIGINT NULL,
-  `osm_type` VARCHAR(1) NULL,
+  `name` VARCHAR(128) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `UN_COORDS` (`latitude` ASC, `longitude` ASC) INVISIBLE,
   INDEX `IN_COUNTRY` (`country` ASC) INVISIBLE,
